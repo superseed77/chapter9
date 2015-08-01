3baac53d48089cd7560f1934a2526982c2064349
yann <whysee77@yahoo.com>
2015-08-01 19:15:59 +0200

    Changed again again

--- StudentGrader.java
+++ StudentGrader.java
@@ -1,6 +1,6 @@
 // A program from Chapter 9 of Sams Teach Yourself Java in 24 Hours
 // by Rogers Cadenhead, http://www.java24hours.com/
-
+//ojooijoij
 package com.java24hours;
 
 class StudentGrader {
